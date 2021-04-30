cdf3c2a5cf4abfcad6379b76c63138afd179221a
Jose Rondon <jose_rond@msn.com>
2021-04-30 19:19:54 -0400

    commit

---
+++ myScript-C.js
@@ -0,0 +1,23 @@
+function changeButton1()    {
+	var x = document.getElementById("previous1");
+	x.setAttribute("style","color: white;background-color: var(--orange)");
+    document.getElementById("table1").setAttribute("style","display: table;");
+    document.getElementById("table2").setAttribute("style","display: none;");
+    document.getElementById("table3").setAttribute("style","display: none;");
+}
+
+function changeButton2() {
+	var x = document.getElementById("previous1");
+	x.setAttribute("style","color: var(--light-gray);Background-color: var(--mid-gray)");
+    document.getElementById("table1").setAttribute("style","display: none;");
+    document.getElementById("table2").setAttribute("style","display: table;");
+    document.getElementById("table3").setAttribute("style","display: none");
+}
+
+function changeButton3() {
+	var x = document.getElementById("previous1");
+	x.setAttribute("style","color: var(--light-gray);Background-color: var(--mid-gray)");
+    document.getElementById("table1").setAttribute("style","display: none;");
+    document.getElementById("table2").setAttribute("style","display: none;");
+    document.getElementById("table3").setAttribute("style","display: flex;");
+}
--- myScript.js
+++ myScript.js
@@ -4,54 +4,45 @@ document.getElementById("button2-2").addEventListener("click", stopSlide);
 var slide = null;
 
 function stopSlide() {
-    clearInterval(slide);
+	clearInterval(slide);
 }
 
 function slideshow1() {
-    var x = document.getElementById("slideshow");
-    var y = x.scrollWidth;
-    var z = y / 4;
-    if (x.scrollLeft + z == x.scrollWidth) {
-        x.scroll({left:(x.scrollWidth * -1), behavior:'smooth'})
-    }
-    else {
-        x.scrollBy({left:(z), behavior:'smooth'});
-    }
+	var x = document.getElementById("slideshow");
+	var y = x.scrollWidth;
+	var z = y / 4;
+	if (x.scrollLeft + z == x.scrollWidth) {
+		x.scroll({left:(x.scrollWidth * -1), behavior:'smooth'})
+	}
+	else {
+		x.scrollBy({left:(z), behavior:'smooth'});
+	}
 }
 
 onload = function () {
-    slide = setInterval(slideshow1, 5000);
+	slide = setInterval(slideshow1, 5000);
 }
 
 function slideshow() {
-    slide = setInterval(slideshow1, 5000);
-    document.getElementById("button1-2").setAttribute("style","display: none;");
-    document.getElementById("counter").setAttribute("style","display: block;");
+	slide = setInterval(slideshow1, 5000);
+	document.getElementById("button1-2").setAttribute("style","display: none;");
+	document.getElementById("counter").setAttribute("style","display: block;");
 }
 
 function scrollSlideF()  {
-    var x = document.getElementById("slideshow");
-    var y = x.scrollWidth;
-    var z = y / 4;
-    x.scrollBy({left:(z),behavior:'smooth'});
-    document.getElementById("button1-2").setAttribute("style","display:block;");
-    document.getElementById("counter").setAttribute("style","display: none;");
+	var x = document.getElementById("slideshow");
+	var y = x.scrollWidth;
+	var z = y / 4;
+	x.scrollBy({left:(z),behavior:'smooth'});
+	document.getElementById("button1-2").setAttribute("style","display:block;");
+	document.getElementById("counter").setAttribute("style","display: none;");
 }
 
 function scrollSlideR()  {
-    var x = document.getElementById("slideshow");
-    var y = x.scrollWidth;
-    var z = (y / 4) * -1;
-    x.scrollBy({left:(z),behavior:'smooth'});
-    document.getElementById("button1-2").setAttribute("style","display:block;");
-    document.getElementById("counter").setAttribute("style","display: none;");
-}
-
-function changeButton() {
-    var x = document.getElementById("previus");
-    x.setAttribute("style","color: var(--light-gray);Background-color: var(--mid-gray)");
-    }
-function changeButton1()    {
-    var x = document.getElementById("previus");
-    x.setAttribute("style","color: white;background-color: var(--orange)");
+	var x = document.getElementById("slideshow");
+	var y = x.scrollWidth;
+	var z = (y / 4) * -1;
+	x.scrollBy({left:(z),behavior:'smooth'});
+	document.getElementById("button1-2").setAttribute("style","display:block;");
+	document.getElementById("counter").setAttribute("style","display: none;");
 }
